4dce068e6816e8ef09d742c9a401355162796dca
luisfelipe23 <luisfelipedomingosvieira@gmail.com>
2022-10-03 21:07:27 -0300

    Project completed

--- main.cpp
+++ main.cpp
@@ -4,81 +4,101 @@
 #include <windows.h>
 #include <iostream>
 
-float RotationSpeed = 0;
-float RotationSpeed1 = 0;
-float RotationSpeed2 = 0;
-float RotationSpeed3 = 0;
+float rotacao1 = 0.0;
+float rotacao2 = 0.0;
+float rotacao3 = 0.0;
+float rotacao4 = 0.0;
 
-float Self_angle = 0;
+float proprioE = 0;
 
-void CreateCircle(float radius)
+float velMercurio = 0.13;
+float velVenus = 0.15;
+float velTerra = 0.28;
+float velMarte = 0.35;
+
+float zoom = -60.0;
+
+//Cria os Planetas
+void CreateCircle(float raio)
 {
-    float _angle = 0;
+    float angulo = 0;
     glLineWidth(3.0);
     glColor3f(0, 0, 0);
     glBegin(GL_LINE_LOOP);
     	for(int i = 0; i<100; i++){
-    		_angle = 2*3.1416*i/100; //2 x pi x r
-    		glVertex3f(radius*cos(_angle), 0, radius*sin(_angle));
+    		angulo = 2*3.1416*i/100; //2 x pi x r
+    		glVertex3f(raio*cos(angulo), 0, raio*sin(angulo));
 		}
 	glEnd();
 }
 
+void pintarSol(float x, float y, float z) {
+    glColor3f(x, y, z);
+}
 
 void display() {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    gluLookAt(-60.0, 40.0, 0, 0, 0, 0, 0, 1.0, 0);
+    gluLookAt(zoom, 40.0, 0, 0, -5.0, 0, 0, 1.0, 0.0);
 
-    glPushMatrix();
-    glDisable(GL_LIGHTING);
-    glColor3f(1, 0.7, 0);
-    glRotatef(Self_angle, 0, 1.0, 0);
-    glutSolidSphere(6.5, 15.0, 15.0);
-    glEnable(GL_LIGHTING);
-    glPopMatrix();
-
-
-	CreateCircle(12.0);
+    //Marte
+    CreateCircle(35.0);
 	glPushMatrix();
-	glTranslatef(12.0*cos(RotationSpeed/0.13), 0, 12.0*sin(RotationSpeed/0.13));
-	glRotatef(Self_angle, 0, 1.0, 0);
+	glTranslatef(35.0*cos(rotacao4/velMarte), 0, 35.0*sin(rotacao4/velMarte));
+    glRotatef(proprioE, 0, 1.0, 0);
     glDisable(GL_LIGHTING);
-    glColor3f(0.4, 0.4, 0.4);
-    glutSolidSphere(1.5, 15.0, 15.0);
+    glColor3f(1.0, 0.2, 0);
+    glutSolidSphere(3.0, 15.0, 15.0);
     glEnable(GL_LIGHTING);
     glPopMatrix();
 
-    CreateCircle(19.0);
-	glPushMatrix();
-	glTranslatef(19.0*cos(RotationSpeed1/0.15), 0, 19.0*sin(RotationSpeed1/0.15));
-    glRotatef(Self_angle, 0, 1.0, 0);
-    glDisable(GL_LIGHTING);
-    glColor3f(1, 0.6, 0);
-    glutSolidSphere(3.3, 15.0, 15.0);
-    glEnable(GL_LIGHTING);
-    glPopMatrix();
-
+    //Terra
     CreateCircle(27.0);
 	glPushMatrix();
-	glTranslatef(27.0*cos(RotationSpeed/0.28), 0, 27.0*sin(RotationSpeed/0.28));
-    glRotatef(Self_angle, 0, 1.0, 0);
+	glTranslatef(27.0*cos(rotacao3/velTerra), 0, 27.0*sin(rotacao3/velTerra));
+    glRotatef(proprioE, 0, 1.0, 0);
     glDisable(GL_LIGHTING);
     glColor3f(0, 0.5, 0.9);
     glutSolidSphere(3.7, 15.0, 15.0);
     glEnable(GL_LIGHTING);
     glPopMatrix();
 
-    CreateCircle(35.0);
+    //Venus
+    CreateCircle(19.0);
 	glPushMatrix();
-	glTranslatef(35.0*cos(RotationSpeed/0.35), 0, 35.0*sin(RotationSpeed/0.35));
-    glRotatef(Self_angle, 0, 1.0, 0);
+	glTranslatef(19.0*cos(rotacao2/velVenus), 0, 19.0*sin(rotacao2/velVenus));
+    glRotatef(proprioE, 0, 1.0, 0);
     glDisable(GL_LIGHTING);
-    glColor3f(1.0, 0.2, 0);
-    glutSolidSphere(3.0, 15.0, 15.0);
+    glColor3f(1, 0.6, 0);
+    glutSolidSphere(3.3, 15.0, 15.0);
     glEnable(GL_LIGHTING);
     glPopMatrix();
+
+    //Mercurio
+    CreateCircle(12.0);
+	glPushMatrix();
+	glTranslatef(12.0*cos(rotacao1/velMercurio), 0, 12.0*sin(rotacao1/velMercurio));
+	glRotatef(proprioE, 0, 1.0, 0);
+    glDisable(GL_LIGHTING);
+    glColor3f(0.4, 0.4, 0.4);
+    glutSolidSphere(1.5, 15.0, 15.0);
+    glEnable(GL_LIGHTING);
+    glPopMatrix();
+
+    //Sol
+    glPushMatrix();
+    glDisable(GL_LIGHTING);
+    if ((velMercurio <= 0.01 && velMercurio >= -0.01) || (velVenus <= 0.01 && velVenus >= -0.01)) {
+        glColor3f(0, 0, 0);
+    }
+    else
+        pintarSol(1, 0.7, 0);
+    glRotatef(proprioE, 0, 1.0, 0);
+    glutSolidSphere(6.5, 15.0, 15.0);
+    glEnable(GL_LIGHTING);
+    glPopMatrix();
+
     glutSwapBuffers();
 }
 
@@ -101,20 +121,47 @@ void init()
 
 }
 
-void key_callback(unsigned char key, int x, int y){
+
+void KeyboardCallback(unsigned char key, int x, int y){
 
     switch(key){
     	case 27:
+    	    printf("Sair");
     		exit(0);
+
+        case 32: {
+            printf("%.2f, %.2f, %.2f, %.2f\n\n", velMercurio, velVenus, velTerra, velMarte);
+            velMercurio = velMercurio - 0.006;
+            velVenus = velVenus - 0.006;
+            velTerra = velTerra - 0.006;
+            velMarte = velMarte - 0.006;
+
+            glutPostRedisplay();
+            break;
+        }
 	}
 }
 
-void spining(void){
+void AumentarZoom (int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON) {
+        if (state == 0) {
+            zoom += 5.0;
+        }
+    }
+    if (button == GLUT_RIGHT_BUTTON) {
+        if (state == 0) {
+            zoom -= 5.0;
+        }
+    }
+}
 
-	RotationSpeed = RotationSpeed + 0.0006;
-	RotationSpeed1 = RotationSpeed1 + 0.0004;
-	RotationSpeed2 = RotationSpeed2 + 0.0002;
-	RotationSpeed3 = RotationSpeed3 + 0.0001;
+//retoção dos planetas no sol
+void Rotacao(void){
+
+	rotacao1 = rotacao1 + 0.0006;
+	rotacao2 = rotacao2 + 0.0004;
+	rotacao3 = rotacao3 + 0.0002;
+	rotacao4 = rotacao4 + 0.0001;
 	glutPostRedisplay();
 }
 
@@ -129,15 +176,15 @@ void reshaping(int w, int h){
 
 }
 
+//Rotação no proprio eixo
+void Eixo(int x){
 
-void spining(int x){
-
-	Self_angle = Self_angle + 1.5;
-	if(Self_angle > 360){
-		Self_angle = -360;
+	proprioE = proprioE + 1.5;
+	if(proprioE > 360){
+		proprioE = -360;
 	}
 	glutPostRedisplay();
-	glutTimerFunc(25, spining, 0);
+	glutTimerFunc(25, Eixo, 0);
 }
 
 int main(int argc, char* argv[])
@@ -148,16 +195,17 @@ int main(int argc, char* argv[])
     glutInitWindowPosition(50, 50);
     glutCreateWindow("Solar System");
 
-
     glutDisplayFunc(display);
 
     glutReshapeFunc(reshaping);
 
-    glutIdleFunc(spining);
+    glutIdleFunc(Rotacao);
 
-    glutKeyboardFunc(key_callback);
+    glutKeyboardFunc(KeyboardCallback);
 
-    glutTimerFunc(25, spining, 0);
+    glutMouseFunc(AumentarZoom);
+
+    glutTimerFunc(25, Eixo, 0);
 
     glutMainLoop();
 
